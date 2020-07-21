911b2f1130dd3d6d98249e8bdfaf993041044e87
Tanner Leigh <tannerhleigh@gmail.com>
2020-07-20 22:44:50 -0700

    [js problems] finish assignment

--- problems.js
+++ problems.js
@@ -1,13 +1,13 @@
 //isInside
 function isInside(array, ele) {
-    let element = isInside.indexOf("ele")
+    let element = array.indexOf("ele");
     if (element <= 0) {
         return true;
     } else {
         return false;
     }
 }
-isInside(['taylor', 'rome', 'adam'], 'ele');
+console.log(isInside(['taylor', 'rome', 'adam'], 'ele'));
 
 //reverseStr
 function reverseStr(str) {
@@ -16,7 +16,7 @@ function reverseStr(str) {
     let joinArray = reverseArray.join("");
     return joinArray;
 }
-reverseStr('bootcamp');
+console.log(reverseStr("bootcamp"));
 
 //luckySevens
 function luckySevens(max) {
@@ -25,11 +25,12 @@ function luckySevens(max) {
     for (let i = 7; i >= max; i+=7) {
 
         if(num % 7 === 0) {
-            result.push(i)
+            result.push[i];
          }
     return result;
 }
-luckySevens(25); // => [ 7, 14, 21 ]
+}
+console.log(luckySevens(25)); // => [ 7, 14, 21 ]
 
 //copyMachine
 function copyMachine(element, num) {
@@ -39,32 +40,58 @@ function copyMachine(element, num) {
      }
     return result;
 }
-copyMachine('candy', 2)
-copyMachine('bread', 4)
-copyMachine(11, 6)
+console.log(copyMachine('candy', 2));
 
 //everyOtherWord
 function everyOtherWord(sentence) {
-    let result [];
-    let splitSentence = sentence.splt(" "); //makes an array for every word
+    let result = [];
+    let splitSentence = sentence.split(" "); //makes an array for every word
     for (let i = 0; i < splitSentence.length; i++) {
         if(splitSentence[i] % 2 === 0) {
             result.push(splitSentence);
         }
     }
     return result;
-
 }
 everyOtherWord('hello how are you doing on this lovely day?'); // => [ 'hello', 'are', 'doing', 'this', 'day?' ]
 
 //word Yeller
 function wordYeller(sentence) {
-    let splitSentence = sent.split(" ");
-    for(let i = 0; i < splitSentence.length; i++) {
-        if()
-    }
+    let removeExclamation = sent.split("!").join("");
+    let newSentence = removeExclamation.split(" ");
+    let finalSentence = newSentence.join("! ");
+    return finalSentence;
 }
-
 let sent = "Stop it now! Please, wont you stop?";
-let yelledWords = wordYeller(sent);
-yelledWords === "Stop! it! now! Please, wont! you! stop?"; // => true
+console.log(wordYeller(sent));
+
+//arraySubstring
+function arraySubstring(words, str) {
+    let newString = words.join(" ");
+    let newArray = [];
+    console.log(newString);
+    for(let i = 0; i < words.length; i ++) {
+        if(str === newString.substring(0, str.length)) {
+            newArray.push(true);
+        } else {
+            newArray.push(false);
+        }
+    }
+    return newArray;
+}
+console.log(arraySubstring(["hello", "history", "helix", "hellos"],"hel"));
+
+//evenCaps
+function evenCaps(sentence) {
+    let result = [];
+    let newSentence = sentence.split("");
+    console.log(newSentence);
+    for(let i = 0; i < newSentence.length; i++) {
+        let capsLetter =  newSentence[i];
+        if(newSentence.indexOf(capsLetter) % 2 === 0) {
+            result.push(capsLetter.toUpperCase());
+        }
+    }
+    result.join(" ");
+}
+console.log(evenCaps("Tom got a small piece of pie"));
